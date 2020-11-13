a7d4017b06c01be78a119f9d2d3a5616c70b7918
monika1020 <65161037+monika1020@users.noreply.github.com>
2020-11-12 19:36:55 -0800

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,44 @@
+/*
+ 4. Given an integer array A of size N, find the sum of elements in it.
+Input: First line contains an integer denoting the test cases 'T'. T test cases follow.
+Each test case contains two lines of input. First line contains N the size of the array A.
+The second line contains the elements of the array.
+Output: For each test case, print the sum of all elements of the array in separate lines.
+ Example:Input:
+2
+3
+3 2 1
+4
+1 2 3 4
+Output:
+6
+10
+ */
+
+package Assignment1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			int num=sc.nextInt();
+			ArrayList<Integer>a=new ArrayList<Integer>();
+			for(int j=0;j<num;j++) {
+				int m=sc.nextInt();
+				a.add(m);
+				
+			}
+			int sum=0;
+			for(int j=0;j<num;j++) {
+				sum+=a.get(j);
+			}
+			System.out.println(sum);
+			
+		}
+	}
+}
